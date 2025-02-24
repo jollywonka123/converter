3d2571aceac85eab3b9087a7102ddc034008e58c
Добра Виктор <81097869+jollywonka123@users.noreply.github.com>
2025-02-25 00:20:06 +0300

    Update entrypoint.c

--- entrypoint.c
+++ entrypoint.c
@@ -1,17 +1,27 @@
 #include "entrypoint.h"
 #include "logic.h"
+#include <stdarg.h>
 
 
-void doOperation(enum Actions action, AppContext *context, const char *newText, int inputSystem, int outputSystem) {
+void doOperation(enum Actions action, AppContext *context, ...) {
+    va_list args;
+    va_start(args, context);
     switch (action) {
-    case initialization:
+    case initialization: {
+        int inputSystem = va_arg(args, int);
+        int outputSystem = va_arg(args, int);
         initialize(context, inputSystem, outputSystem);
+    }
         break;
-    case textUpdating:
+    case textUpdating: {
+        char* newText = va_arg(args, char*);
         updateInputText(context, newText);
+    }
         break;
-    case checkInputs:
-        checkTextCorrect(context, newText);
+    case checkInputs: {
+        char* newText1 = va_arg(args, char*);
+        checkTextCorrect(context, newText1);
+    }
         break;
     case convert:
         convertToBase(context);
@@ -19,4 +29,5 @@ void doOperation(enum Actions action, AppContext *context, const char *newText, 
     default:
         break;
     }
+    va_end(args);
 }
